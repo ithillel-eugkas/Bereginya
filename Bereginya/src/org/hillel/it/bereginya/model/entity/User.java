c0184fb2b7c9e169cdcabfc784ad0f810056ca0a
Konstantin <highman@ua.fm>
2013-03-24 22:04:02 +0200

    Test on GitHub

--- Bereginya/src/org/hillel/it/bereginya/model/entity/User.java
+++ Bereginya/src/org/hillel/it/bereginya/model/entity/User.java
@@ -1,5 +1,9 @@
 package org.hillel.it.bereginya.model.entity;
 
+//my first user class
 public class User {
-
+	public User() {
+		//Проверка Push на GitHub
+		System.out.println("Default constructor");
+	}
 }
